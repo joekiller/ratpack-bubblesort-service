c61f27557ac07729fae8110384a703ca3170cece
Joseph Lawson <joe@joekiller.com>
2015-12-22 10:44:45 -0500

    update scripts. make asg scale up on heavier load

--- scripts/traffic.bash
+++ scripts/traffic.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export TARGET_HOST=
+
 traffic () {
     while true; do
         n1=$[ ( $RANDOM % 10 )  + 1 ]
@@ -7,34 +9,44 @@ traffic () {
         n3=$[ ( $RANDOM % 10 )  + 1 ]
         n4=$[ ( $RANDOM % 10 )  + 1 ]
         echo -n "[${n1},${n2},${n3},${n4}] -> "
-        echo $(curl -s -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "[${n1},${n2},${n3},${n4}]" 'http://localhost:5050/sort')
-        sleep $[ ( $RANDOM % 10 )  + 1 ]s
+        echo $(curl -s -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "[${n1},${n2},${n3},${n4}]" ${TARGET_HOST}'/sort')
     done
 }
 
 bakepie () {
-    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" 'http://localhost:5050/bakepie')
+    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" ${TARGET_HOST}'/bakepie')
 }
 
 stoppie () {
-    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" 'http://localhost:5050/stoppie')
+    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" ${TARGET_HOST}'/stoppie')
 }
 
 bomb () {
-    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" 'http://localhost:5050/armbomb')
+    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" ${TARGET_HOST}'/armbomb')
     healthcheck
 }
 
 healthcheck () {
-    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" 'http://localhost:5050/')
+    echo $(curl -s -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" ${TARGET_HOST}'/')
 }
 start () {
     echo "Run the following you silly goose."
     echo "./gradlew run"
 }
 
+whichhost () {
+    echo "Current host is: ${TARGET_HOST}"
+}
+
+targetlocal () {
+    TARGET_HOST='http://localhost:5050'
+}
+
+targetelb () {
+    TARGET_HOST=$1
+}
 demohelp () {
-    echo "Commands: traffic, bakepie, stoppie, bomb, healthcheck, start"
+    echo "Commands: traffic, bakepie, stoppie, bomb, healthcheck, start, whichhost, targetlocal, targetelb"
 }
 
 demohelp
